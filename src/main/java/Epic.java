0b429a088fab81de44403c69cf851ac8b816be80
Neto test <zilya.zakirova1991@gmail.com>
2023-05-03 20:22:03 +0300

    Adds6

--- src/main/java/Epic.java
+++ src/main/java/Epic.java
@@ -1,6 +1,6 @@
 public class Epic extends Task{
     private String [] subtasks;
-    public Epic(int id, String title, String[] subtasks){
+    public Epic(int id, String[] subtasks){
         super(id);
         this.subtasks=subtasks;
     }
--- src/main/java/Meeting.java
+++ src/main/java/Meeting.java
@@ -3,7 +3,7 @@ public class Meeting extends Task {
     private String project;
     private String start;
 
-    public Meeting(int id, String title,String topic,String project,String start) {
+    public Meeting(int id,String topic,String project,String start) {
         super(id);
         this.topic=topic;
         this.project=project;
